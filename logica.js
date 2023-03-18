da6125ef7fe67748b25397f379961eea181262a4
JGIL <bless.juan@hotmail.com>
2023-03-17 23:14:07 -0300

    asd

--- logica.js
+++ logica.js
@@ -1,9 +1,12 @@
 "use strict";
 
-$.ajax({
-  url: "https://randomuser.me/api/",
-  dataType: "json",
-  success: function (data) {
-    console.log(data);
-  },
-});
+fetch("https://randomuser.me/api/")
+  .then((response) => response.json())
+  .then((data) => {
+    const results = data.results[0];
+    console.log(results);
+    document.getElementById(
+      "nombre"
+    ).innerHTML = `CV de ${results.name.title} ${results.name.first} ${results.name.last}`;
+  })
+  .catch((error) => console.error(error));
